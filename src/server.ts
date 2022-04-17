ec62ffb383992c55a410e27de07c818b432ef0ad
thalles-victor <thalles.victor2012@gmail.com>
2022-04-17 10:43:09 -0300

    Reorganizado o codigo

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,26 @@
 import express, { Request, Response } from 'express';
 import { graphqlHTTP } from 'express-graphql';
 
-
-const app = express();
-const PORT = process.env.PORT || 3000 || 4000
-
 /* PostRouters by Rest API */
-import { postRouter, getPostById, addPost } from './routers/PostsRouter';
+import {
+  postRouter,
+  getPostById,
+  addPost
+} from './routers/PostsRouter';
 /* ----------- */
 
 /* Graphql ultilities */
 import { resolvers } from './graphql/Resolvres/resolver';
 import { schema } from './graphql/Shemas/shema';
+/* ------------------ */
+
+
+const PORT = process.env.PORT || 3000 || 4000
+const app = express();
 
 const booStrap = async () => {
   app.get('/', (req: Request, res: Response) => {
-    res.status(200).send("Hello my frend")
+    res.status(200).send("Hello, I'm root router 😉")
   })
 
   app.use(postRouter);
